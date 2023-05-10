2432085b3dda7fc7a0300037a52fc9aa85b82790
Ayoze Martin <ayozemt@gmail.com>
2023-05-10 20:19:32 +0200

    travel details router

---
+++ routes/task.routes.js
@@ -0,0 +1,16 @@
+const router = require("express").Router();
+const Task = require("../models/Task.model");
+const Travel = require("../models/Travel.model");
+const User = require("../models/User.model");
+
+
+//Create tasks with API OpenTripMap data
+
+
+
+
+
+
+
+
+module.exports = router;
