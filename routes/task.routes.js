c3b01ca4517a7118b250ee329c4ac1d4b2f76853
SalvadorBellver <salvador@salvadorbellver.com>
2023-05-17 19:51:07 +0200

    Opentask updated

--- routes/task.routes.js
+++ routes/task.routes.js
@@ -7,52 +7,51 @@ const apiKey = process.env.OPENTRIPMAP;
 
 //Create task form
 router.get("/travel-list/:travelId/create-task", isLoggedIn, (req, res, next) => {
-  const { travelId } = req.params;
+    const { travelId } = req.params;
 
-  Travel.findById(travelId)
-    .then((travel) => {
-      res.render("tasks/create-task", { travel });
-    })
-    .catch((error) => next(error));
+    Travel.findById(travelId)
+        .then((travel) => {
+            res.render("tasks/create-task", { travel, apiKey });
+        })
+        .catch((error) => next(error));
 });
 
 router.post("/travel-list/:travelId/create-task", isLoggedIn, (req, res, next) => {
-  const { task, date, status, price } = req.body;
-  const { travelId } = req.params;
+    const { task, date, status, price } = req.body;
+    const { travelId } = req.params;
 
-  if (!task || !date || !status || !price) {
-    res.render("tasks/create-task", {
-      errorMessage:
-        "All fields are mandatory. Please provide task, date, status and price.",
-    });
-    return;
-  }
+    if (!task || !date || !status || !price) {
+        res.render("tasks/create-task", {
+            errorMessage: "All fields are mandatory. Please provide task, date, status and price.",
+        });
+        return;
+    }
 
-  Task.create({
-    task,
-    date,
-    status,
-    price,
-    travel: travelId,
-  })
-    .then((task) => {
-      console.log("Newly created task is: ", task);
-      res.redirect(`/travel-list/${travelId}`);
-    })
-    .catch((error) => next(error));
+    Task.create({
+            task,
+            date,
+            status,
+            price,
+            travel: travelId,
+        })
+        .then((task) => {
+            console.log("Newly created task is: ", task);
+            res.redirect(`/travel-list/${travelId}`);
+        })
+        .catch((error) => next(error));
 });
 
 //Delete task
 router.post("/travel-list/:travelId/delete-task/:taskId", isLoggedIn, (req, res, next) => {
-  const { travelId, taskId } = req.params;
+    const { travelId, taskId } = req.params;
 
-  Task.findByIdAndDelete(taskId)
-    .then(() => res.redirect(`/travel-list/${travelId}`))
-    .catch((error) => {
-      console.log("Error while deleting task: ", error);
+    Task.findByIdAndDelete(taskId)
+        .then(() => res.redirect(`/travel-list/${travelId}`))
+        .catch((error) => {
+            console.log("Error while deleting task: ", error);
 
-      next(error);
-    });
+            next(error);
+        });
 });
 
 //Search tasks with API OpenTripMap data
